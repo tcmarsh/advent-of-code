13565061d7ed85c836c10ea8e0d3748a57b66a92
Trevor Marsh <gatheringshadow@gmail.com>
2016-01-09 10:21:40 -0700

    Assign a number to a variable

--- src/seven/Day7.java
+++ src/seven/Day7.java
@@ -1,7 +1,23 @@
 package seven;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by trevor on 12/29/15.
  */
 public class Day7 {
+    private Map<String, Integer> variables = new HashMap<>();
+
+    public Day7(String assignment) {
+        String[] parts = assignment.split(" ");
+        variables.put(parts[2], Integer.parseInt(parts[0]));
+    }
+
+    public int getVariable(String variableName) {
+        return variables.get(variableName);
+    }
 }
--- src/seven/Day7Test.java
+++ src/seven/Day7Test.java
@@ -1,7 +1,18 @@
 package seven;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by trevor on 12/29/15.
  */
 public class Day7Test {
+    @Test
+    public void testArrowAssignsNumber() {
+        Day7 test = new Day7("123 -> x");
+        Assert.assertEquals(123, test.getVariable("x"));
+
+        test = new Day7("456 -> y");
+        Assert.assertEquals(456, test.getVariable("y"));
+    }
 }

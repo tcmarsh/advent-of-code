1a5c9b5f6fec3206ba17365063f0075ec0e6f1b2
Trevor Marsh <gatheringshadow@gmail.com>
2016-01-09 22:05:58 -0700

    Add advent listed test case

--- src/seven/Day7Test.java
+++ src/seven/Day7Test.java
@@ -8,7 +8,7 @@ import org.junit.Test;
  */
 public class Day7Test {
     @Test
-    public void testArrowAssignsNumber() {
+    public void arrowAssignsNumber() {
         Day7 test = new Day7("123 -> x");
         Assert.assertEquals(123, test.getVariable("x"));
 
@@ -17,14 +17,14 @@ public class Day7Test {
     }
 
     @Test
-    public void testMultipleLineNumberAssignment() {
+    public void multipleLineNumberAssignment() {
         Day7 test = new Day7("123 -> x\n456 -> y");
         Assert.assertEquals(123, test.getVariable("x"));
         Assert.assertEquals(456, test.getVariable("y"));
     }
 
     @Test
-    public void testArrowAssignsAnd() {
+    public void arrowAssignsAnd() {
         Day7 test = new Day7(
                 "123 -> x\n" +
                 "456 -> y\n" +
@@ -33,7 +33,7 @@ public class Day7Test {
     }
 
     @Test
-    public void testArrowAssignsOr() {
+    public void arrowAssignsOr() {
         Day7 test = new Day7(
                 "123 -> x\n" +
                 "456 -> y\n" +
@@ -42,19 +42,19 @@ public class Day7Test {
     }
 
     @Test
-    public void testArrowAssignsLeftShift() {
+    public void arrowAssignsLeftShift() {
         Day7 test = new Day7("123 -> x\nx LSHIFT 2 -> d");
         Assert.assertEquals(492, test.getVariable("d"));
     }
 
     @Test
-    public void testArrowAssignsRightShift() {
+    public void arrowAssignsRightShift() {
         Day7 test = new Day7("456 -> y\ny RSHIFT 2 -> d");
         Assert.assertEquals(114, test.getVariable("d"));
     }
 
     @Test
-    public void testArrowAssignsNot() {
+    public void arrowAssignsNot() {
         Day7 test = new Day7(
                 "123 -> x\n" +
                 "456 -> y\n" +
@@ -62,5 +62,26 @@ public class Day7Test {
                 "NOT y -> e\n");
         Assert.assertEquals(65412, test.getVariable("d"));
         Assert.assertEquals(65079, test.getVariable("e"));
+    }
+
+    @Test
+    public void adventTestCase() {
+        Day7 test = new Day7(
+                "123 -> x\n" +
+                "456 -> y\n" +
+                "x AND y -> d\n" +
+                "x OR y -> e\n" +
+                "x LSHIFT 2 -> f\n" +
+                "y RSHIFT 2 -> g\n" +
+                "NOT x -> h\n" +
+                "NOT y -> i");
+        Assert.assertEquals(72, test.getVariable("d"));
+        Assert.assertEquals(507, test.getVariable("e"));
+        Assert.assertEquals(492, test.getVariable("f"));
+        Assert.assertEquals(114, test.getVariable("g"));
+        Assert.assertEquals(65412, test.getVariable("h"));
+        Assert.assertEquals(65079, test.getVariable("i"));
+        Assert.assertEquals(123, test.getVariable("x"));
+        Assert.assertEquals(456, test.getVariable("y"));
     }
 }
